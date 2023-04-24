f0de97369a6e94409d59687f5328ea48a71d526f
sungjunminn <msj3001@naver.com>
2023-04-24 14:21:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
